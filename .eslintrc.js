356bf3bd570db29fe45179b46c65b3657d6c4dde
kinghat <kinghat@protonmail.com>
2019-04-08 16:20:14 -0500

    update configs and deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,35 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true,
+    webextensions: true
   },
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
+    'airbnb-base',
+    'plugin:prettier/recommended'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-inline-comments": "off",
+        "no-undef": "warn",
+        "no-unused-vars": "warn",
+        "no-use-before-define": "off",
+        "no-restricted-syntax": [
+            "off",
+            {
+                "selector": "ForOfStatement",
+                "message": "frowned upon using For...Of"
+            }
+        ],
+        "prettier/prettier": [
+            "error",
+            {
+                "printWidth": 120
+            }
+        ]
   },
   parserOptions: {
     parser: 'babel-eslint',
