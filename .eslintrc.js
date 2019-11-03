b2f217870a08f5415fd23483cdf13cae5edc7a91
kinghat <kinghat@protonmail.com>
2019-11-03 15:24:32 -0600

    changed eslint & prettier setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,51 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: ["plugin:vue/essential", "@vue/airbnb", "plugin:prettier/recommended"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-unused-expressions": "off",
-    "no-param-reassign": "off",
-    "import/no-extraneous-dependencies": ["off"],
-    // "import/no-extraneous-dependencies": [
-    //   "warn",
-    //   { devDependencies: false, optionalDependencies: false, peerDependencies: false },
-    // ],
-    "no-unused-vars": ["off"],
-    // "no-unused-vars": ["off", { vars: "all", args: "after-used", ignoreRestSiblings: false }],
-    "prettier/prettier": [
-      "error",
-      {
-        printWidth: 120,
-        singleQuote: false,
-        trailingComma: "all",
-      },
+    "env": {
+        "node": true,
+        "browser": true,
+        "es6": true,
+    },
+    "extends": [
+        "plugin:vue/essential",
+        "@vue/airbnb",
+        "airbnb-base",
+        "plugin:prettier/recommended"
     ],
-  },
-  parserOptions: {
-    parser: "babel-eslint",
-  },
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module",
+        parser: "babel-eslint",
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+      "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+      "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+      "no-unused-expressions": "off",
+      "no-param-reassign": "off",
+      "import/no-extraneous-dependencies": ["off"],
+      "no-restricted-syntax": ["off", "ForOfStatement"],
+      // "import/no-extraneous-dependencies": [
+      //   "warn",
+      //   { devDependencies: false, optionalDependencies: false, peerDependencies: false },
+      // ],
+      "no-unused-vars": ["off"],
+      // "no-unused-vars": ["off", { vars: "all", args: "after-used", ignoreRestSiblings: false }],
+        "prettier/prettier": [
+            "error",
+            {
+                "arrowParens": "always",
+                "bracketSpacing": true,
+                "printWidth": 120,
+                "semi": true,
+                "tabWidth": 2,
+                "trailingComma": "all",
+                "useTabs": true
+            }
+        ]
+    }
 };
