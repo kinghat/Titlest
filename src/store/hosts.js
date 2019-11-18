298cd89eb134375244ab6e712e55c26ea2256344
kinghat <kinghat@protonmail.com>
2019-11-17 22:34:03 -0600

    added REMOVE_HOST and REMOVE_HOSTS mutations

--- src/store/hosts.js
+++ src/store/hosts.js
@@ -77,29 +77,42 @@ const actions = {
 		// from the action. :shruggie:
 		if (payload.payload) return;
 		payload.index = getters.getHostIndexByHostName(payload.host.hostName);
-		console.log(`LOG: action => setHostProperty -> payload: `, payload);
+		// console.log(`LOG: action => setHostProperty -> payload: `, payload);
 		commit(payload.mutation, payload);
 	},
 	setHosts({ commit, getters }, payload) {
-		console.log(`LOG: action => setHosts -> payload: `, payload);
+		// console.log(`LOG: action => setHosts -> payload: `, payload);
 		commit("SET_HOSTS", payload);
 	},
 	setHost({ commit, getters }, payload) {
-		console.log(`LOG: action => setHost -> payload: `, payload);
+		// console.log(`LOG: action => setHost -> payload: `, payload);
 		commit("SET_HOST", payload);
 	},
+	removeHost({commit, gett}, payload) {
+      // console.log(`LOG: removeHost -> payload: `, payload);
+		commit("REMOVE_HOST", payload)
+	}
 };
 
 const mutations = {
 	[types.SET_HOST](state, payload) {
-		console.log(`LOG: SET_HOST => payload:`, payload);
+		// console.log(`LOG: SET_HOST => payload:`, payload);
 		state.hosts.push(payload);
-		console.log(`LOG: SET_HOST => state.host: `, state.host);
+		// console.log(`LOG: SET_HOST => state.host: `, state.host);
 	},
 	[types.SET_HOSTS](state, payload) {
-		console.log(`LOG: SET_HOSTS => payload:`, payload);
+		// console.log(`LOG: SET_HOSTS => payload:`, payload);
 		state.hosts = [...payload];
-		console.log(`LOG: SET_HOSTS => state.hosts: `, state.hosts);
+		// console.log(`LOG: SET_HOSTS => state.hosts: `, state.hosts);
+	},
+	[types.REMOVE_HOST](state, payload) {
+   // console.log(`LOG: REMOVE_HOST => payload: `, payload);
+		state.hosts.splice(payload.index, 1);
+	},
+	[types.REMOVE_HOSTS](state, payload) {
+		// console.log(`LOG: SET_HOSTS => payload:`, payload);
+		state.hosts = [...payload];
+		// console.log(`LOG: SET_HOSTS => state.hosts: `, state.hosts);
 	},
 	[types.SET_USER_TITLE](state, payload) {
 		state.hosts[payload.index].userTitle = payload.value;
@@ -108,12 +121,12 @@ const mutations = {
 		state.hosts[payload.index].isAppended = payload.value;
 	},
 	[types.SET_HOST_STATE](state, payload) {
-		console.log(`LOG: mutation => SET_HOST_STATE payload: `, payload);
+		// console.log(`LOG: mutation => SET_HOST_STATE payload: `, payload);
 		state.hosts[payload.index].hostState = payload.value;
-		console.log(`LOG: mutation => SET_HOST_STATE state: `, state);
+		// console.log(`LOG: mutation => SET_HOST_STATE state: `, state);
 	},
 	[types.SET_ORIGINAL_TAB_TITLE](state, payload) {
-		console.log(`LOG: mutation => SET_ORIGINAL_TAB_TITLE payload: `, payload);
+		// console.log(`LOG: mutation => SET_ORIGINAL_TAB_TITLE payload: `, payload);
 		state.hosts[payload.index].originalTabTitles = { ...payload.value };
 	},
 };
--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,5 +1,7 @@
 export const SET_HOST = "SET_HOST";
 export const SET_HOSTS = "SET_HOSTS";
+export const REMOVE_HOST = "REMOVE_HOST";
+export const REMOVE_HOSTS = "REMOVE_HOSTS";
 export const SET_IS_APPENDED = "SET_IS_APPENDED";
 export const SET_USER_TITLE = "SET_USER_TITLE";
 export const SET_HOST_STATE = "SET_HOST_STATE";
